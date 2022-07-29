5549c9154c02d59fee63650deb2c22c8d32cdf34
Viliana Devbunova <55013584+viliana-dev@users.noreply.github.com>
2022-07-29 14:27:58 +0300

    Update quantize_with_kmeans.py

--- examples/textless_nlp/gslm/speech2unit/clustering/quantize_with_kmeans.py
+++ examples/textless_nlp/gslm/speech2unit/clustering/quantize_with_kmeans.py
@@ -6,8 +6,12 @@
 import argparse
 import logging
 import os
+from tqdm.notebook import tqdm
 
 import numpy as np
+from scipy.io import wavfile
+import yt 
+from typing import Union, Dict, Any, TypeVar, Iterable
 
 import joblib
 from examples.textless_nlp.gslm.speech2unit.clustering.utils import (
@@ -67,6 +71,18 @@ def get_parser():
         help="Manifest file containing the root dir and file names",
     )
     parser.add_argument(
+        "--mode",
+        type=str,
+        default='classic',
+        help="How load data and model: classic/huggingface-yt/chpt-yt.",
+    )
+    parser.add_argument(
+        "--norm",
+        type=str,
+        help="Did you normalize the model during training? (only for hugging face model) true/false",
+        default='false',
+    )
+    parser.add_argument(
         "--out_quantized_file_path",
         required=True,
         type=str,
@@ -85,14 +101,28 @@ def main(args, logger):
         features_batch = np.load(args.features_path)
     else:
         logger.info(f"Extracting {args.feature_type} acoustic features...")
-        features_batch = get_features(
-            feature_type=args.feature_type,
-            checkpoint_path=args.acoustic_model_path,
-            layer=args.layer,
-            manifest_path=args.manifest_path,
-            sample_pct=1.0,
-            flatten=False,
-        )
+        if args.mode == 'classic':
+            features_batch = get_features(
+                feature_type=args.feature_type,
+                checkpoint_path=args.acoustic_model_path,
+                layer=args.layer,
+                manifest_path=args.manifest_path,
+                sample_pct=1.0,
+                flatten=False,
+                norm=args.norm,
+                mode=args.mode,
+            )
+        else:
+            ids_batch, features_batch = get_features(
+                feature_type=args.feature_type,
+                checkpoint_path=args.acoustic_model_path,
+                layer=args.layer,
+                manifest_path=args.manifest_path,
+                sample_pct=1.0,
+                flatten=False,
+                norm=args.norm,
+                mode=args.mode,
+            )
         logger.info(
             f"Features extracted for {len(features_batch)} utterances.\n"
         )
@@ -104,17 +134,58 @@ def main(args, logger):
     logger.info(f"Loading K-means model from {args.kmeans_model_path} ...")
     kmeans_model = joblib.load(open(args.kmeans_model_path, "rb"))
     kmeans_model.verbose = False
+    
+    def huberts_utterance(phonemes):
+        phones = [{'phone': str(phonemes[0])}]
+        last = phonemes[0]
+        for i in phonemes[1:]:
+            if i == last:
+                continue
+            phones.append({'phone': str(i)})
+            last = i
+        return {'words': [{'phones': phones, 'text': ""}]}
 
-    _, fnames, _ = get_audio_files(args.manifest_path)
+    if args.mode == 'classic':
+        _, fnames, _ = get_audio_files(args.manifest_path)
 
-    os.makedirs(os.path.dirname(args.out_quantized_file_path), exist_ok=True)
-    print(f"Writing quantized predictions to {args.out_quantized_file_path}")
-    with open(args.out_quantized_file_path, "w") as fout:
-        for i, feats in enumerate(features_batch):
-            pred = kmeans_model.predict(feats)
-            pred_str = " ".join(str(p) for p in pred)
-            base_fname = os.path.basename(fnames[i]).rstrip(args.extension)
-            fout.write(f"{base_fname}|{pred_str}\n")
+        os.makedirs(os.path.dirname(args.out_quantized_file_path), exist_ok=True)
+        print(f"Writing quantized predictions to {args.out_quantized_file_path}")
+        with open(args.out_quantized_file_path, "w") as fout:
+            for i, feats in enumerate(features_batch):
+                pred = kmeans_model.predict(feats)
+                pred_str = " ".join(str(p) for p in pred)
+                base_fname = os.path.basename(fnames[i]).rstrip(args.extension)
+                fout.write(f"{base_fname}|{pred_str}\n")
+    else:
+        schema = [
+            {'name': 'ID', 'type_v3': {'type_name': 'optional', 'item': 'utf8'}},
+            {'name': 'huberts_phonemes', 'type_v3': {'type_name': 'optional', 'item': 'yson'}}
+        ]
+        f = open('/home/viliana-dev/.yt/token')
+        yt_token = f.read().strip()
+        f.close()
+        yt_client = yt.wrapper.YtClient('hahn', token=yt_token)
+        yt_client.create_table(args.out_quantized_file_path, attributes={"schema": schema})
+
+        def row_processor(source_rows: yt.wrapper.format.RowsIterator, n_rows: int) -> Iterable:
+            i = 0
+            for row in tqdm(source_rows, total=n_rows):
+                feats = features_batch[i]
+                assert row['ID'] == ids_batch[i], "Rows must have equal ID"
+                i += 1
+                pred = kmeans_model.predict(feats)
+                phones = huberts_utterance(pred)
+                yield {
+                    "ID": row["ID"],
+                    "huberts_phonemes": phones
+                }
+
+        print(f"Writing quantized predictions to {args.out_quantized_file_path}")
+        source_rows = yt_client.read_table(table=yt.wrapper.TablePath(args.manifest_path))
+        rows_generator = row_processor(source_rows, n_rows=yt_client.row_count(args.manifest_path))
+        yt_client.write_table(args.out_quantized_file_path, rows_generator)
+        yt_client.run_sort(args.out_quantized_file_path, sort_by="ID")
+
 
 
 if __name__ == "__main__":
