c88e0c56326bb47be18264dd9d600e7c20ebab1b
Viliana Devbunova <55013584+viliana-dev@users.noreply.github.com>
2022-08-03 18:56:30 +0300

    Update cluster_kmeans.py

--- examples/textless_nlp/gslm/speech2unit/clustering/cluster_kmeans.py
+++ examples/textless_nlp/gslm/speech2unit/clustering/cluster_kmeans.py
@@ -6,14 +6,18 @@
 import argparse
 import logging
 import os
-import time
+from tqdm.notebook import tqdm
 
 import numpy as np
-from sklearn.cluster import MiniBatchKMeans
+from scipy.io import wavfile
+import yt 
+from typing import Union, Dict, Any, TypeVar, Iterable
 
 import joblib
+from examples.textless_nlp.gslm.speech2unit.clustering.utils import (
+    get_audio_files,
+)
 from examples.textless_nlp.gslm.speech2unit.pretrained.utils import (
-    get_and_dump_features,
     get_features,
 )
 
@@ -27,36 +31,20 @@ def get_logger():
 
 def get_parser():
     parser = argparse.ArgumentParser(
-        description="Learn K-means clustering over acoustic features."
-    )
-
-    # Features arguments
-    parser.add_argument(
-        "--in_features_path", type=str, default=None, help="Features file path"
+        description="Quantize using K-means clustering over acoustic features."
     )
     parser.add_argument(
         "--feature_type",
         type=str,
         choices=["logmel", "hubert", "w2v2", "cpc"],
         default=None,
+        required=True,
         help="Acoustic feature type",
     )
     parser.add_argument(
-        "--manifest_path",
+        "--acoustic_model_path",
         type=str,
-        default=None,
-        help="Manifest file containing the root dir and file names",
-    )
-    parser.add_argument(
-        "--out_features_path",
-        type=str,
-        default=None,
-        help="Features file path to write to",
-    )
-    parser.add_argument(
-        "--checkpoint_path",
-        type=str,
-        help="Pretrained acoustic model checkpoint",
+        help="Pretrained acoustic model checkpoint"
     )
     parser.add_argument(
         "--layer",
@@ -65,41 +53,23 @@ def get_parser():
         default=-1,
     )
     parser.add_argument(
-        "--sample_pct",
-        type=float,
-        help="Percent data to use for K-means training",
-        default=0.1,
-    )
-
-    # K-means arguments
-    parser.add_argument(
-        "--num_clusters", type=int, help="Nubmer of clusters", default=50
-    )
-    parser.add_argument("--init", default="k-means++")
-    parser.add_argument(
-        "--max_iter",
-        type=int,
-        help="Maximum number of iterations for K-means training",
-        default=150,
-    )
-    parser.add_argument(
-        "--batch_size",
-        type=int,
-        help="Batch size for K-means training",
-        default=10000,
-    )
-    parser.add_argument("--tol", default=0.0, type=float)
-    parser.add_argument("--max_no_improvement", default=100, type=int)
-    parser.add_argument("--n_init", default=20, type=int)
-    parser.add_argument("--reassignment_ratio", default=0.5, type=float)
-    parser.add_argument(
-        "--out_kmeans_model_path",
+        "--kmeans_model_path",
         type=str,
         required=True,
-        help="Path to save K-means model",
+        help="K-means model file path to use for inference",
     )
-
-    #model arguments
+    parser.add_argument(
+        "--features_path",
+        type=str,
+        default=None,
+        help="Features file path. You don't need to enter acoustic model details if you have dumped features",
+    )
+    parser.add_argument(
+        "--manifest_path",
+        type=str,
+        default=None,
+        help="Manifest file containing the root dir and file names",
+    )
     parser.add_argument(
         "--mode",
         type=str,
@@ -112,112 +82,122 @@ def get_parser():
         help="Did you normalize the model during training? true/false",
         default='false',
     )
-
-    # Leftovers
     parser.add_argument(
-        "--seed",
-        type=int,
-        help="Random seed to use for K-means training",
-        default=1369,
+        "--yt_token",
+        type=str,
+        help="Your yt_token",
     )
-
+    parser.add_argument(
+        "--out_quantized_file_path",
+        required=True,
+        type=str,
+        help="File path of quantized output.",
+    )
+    parser.add_argument(
+        "--extension", type=str, default=".flac", help="Features file path"
+    )
     return parser
 
 
-def get_kmeans_model(
-    n_clusters,
-    init,
-    max_iter,
-    batch_size,
-    tol,
-    max_no_improvement,
-    n_init,
-    reassignment_ratio,
-    random_state,
-):
-    return MiniBatchKMeans(
-        n_clusters=n_clusters,
-        init=init,
-        max_iter=max_iter,
-        batch_size=batch_size,
-        tol=tol,
-        max_no_improvement=max_no_improvement,
-        n_init=n_init,
-        reassignment_ratio=reassignment_ratio,
-        random_state=random_state,
-        verbose=1,
-        compute_labels=True,
-        init_size=None,
-    )
-
-
-def train_kmeans(kmeans_model, features_batch):
-    start_time = time.time()
-    kmeans_model.fit(features_batch)
-    time_taken = round((time.time() - start_time) // 60, 2)
-    return kmeans_model, time_taken
-
-
 def main(args, logger):
-    # Features loading/extraction for K-means
-    if args.in_features_path:
-        # Feature loading
-        logger.info(f"Loading features from {args.in_features_path}...")
-        features_batch = np.load(args.in_features_path, allow_pickle=True)
+    # Feature extraction
+    if args.features_path is not None:
+        logger.info(f"Loading acoustic features from {args.features_path}...")
+        features_batch = np.load(args.features_path)
     else:
-        # Feature extraction
         logger.info(f"Extracting {args.feature_type} acoustic features...")
-        features_batch = (
-            get_features(
+        if args.mode == 'classic':
+            features_batch = get_features(
                 feature_type=args.feature_type,
-                checkpoint_path=args.checkpoint_path,
+                checkpoint_path=args.acoustic_model_path,
                 layer=args.layer,
                 manifest_path=args.manifest_path,
-                sample_pct=args.sample_pct,
-                flatten=True,
+                sample_pct=1.0,
+                flatten=False,
+                yt_token=args.yt_token,
                 norm=args.norm,
                 mode=args.mode,
             )
-            if not args.out_features_path
-            else get_and_dump_features(
+        else:
+            ids_batch, features_batch = get_features(
                 feature_type=args.feature_type,
-                checkpoint_path=args.checkpoint_path,
+                checkpoint_path=args.acoustic_model_path,
                 layer=args.layer,
                 manifest_path=args.manifest_path,
-                sample_pct=args.sample_pct,
-                flatten=True,
-                out_features_path=args.out_features_path,
+                sample_pct=1.0,
+                flatten=False,
+                yt_token=args.yt_token,
+                norm=args.norm,
+                mode=args.mode,
             )
+        logger.info(
+            f"Features extracted for {len(features_batch)} utterances.\n"
         )
-        if args.out_features_path:
-            logger.info(
-                f"Saved extracted features at {args.out_features_path}"
-            )
-    logger.info(f"Features shape = {features_batch.shape}\n")
+        logger.info(
+            f"Dimensionality of representation = {features_batch[0].shape[1]}"
+        )
 
-    # Learn and save K-means model
-    kmeans_model = get_kmeans_model(
-        n_clusters=args.num_clusters,
-        init=args.init,
-        max_iter=args.max_iter,
-        batch_size=args.batch_size,
-        tol=args.tol,
-        max_no_improvement=args.max_no_improvement,
-        n_init=args.n_init,
-        reassignment_ratio=args.reassignment_ratio,
-        random_state=args.seed,
-    )
-    logger.info("Starting k-means training...")
-    kmeans_model, time_taken = train_kmeans(
-        kmeans_model=kmeans_model, features_batch=features_batch
-    )
-    logger.info(f"...done k-means training in {time_taken} minutes")
-    inertia = -kmeans_model.score(features_batch) / len(features_batch)
-    logger.info(f"Total intertia: {round(inertia, 2)}\n")
+    # K-means model
+    logger.info(f"Loading K-means model from {args.kmeans_model_path} ...")
+    kmeans_model = joblib.load(open(args.kmeans_model_path, "rb"))
+    kmeans_model.verbose = False
+    
+    def huberts_utterance(phonemes):
+        tmp = []
+        for i in phonemes:
+            tmp.append(str(i))
+        return ' '.join(tmp)
 
-    logger.info(f"Saving k-means model to {args.out_kmeans_model_path}")
-    os.makedirs(os.path.dirname(args.out_kmeans_model_path), exist_ok=True)
-    joblib.dump(kmeans_model, open(args.out_kmeans_model_path, "wb"))
+        #phones = [{'phone': str(phonemes[0])}]
+        #last = phonemes[0]
+        #for i in phonemes[1:]:
+        #    if i == last:
+        #        continue
+        #    phones.append({'phone': str(i)})
+        #    last = i
+        #return {'words': [{'phones': phones, 'text': ""}]}
+
+    if args.mode == 'classic':
+        _, fnames, _ = get_audio_files(args.manifest_path)
+
+        os.makedirs(os.path.dirname(args.out_quantized_file_path), exist_ok=True)
+        print(f"Writing quantized predictions to {args.out_quantized_file_path}")
+        with open(args.out_quantized_file_path, "w") as fout:
+            for i, feats in enumerate(features_batch):
+                pred = kmeans_model.predict(feats)
+                pred_str = " ".join(str(p) for p in pred)
+                base_fname = os.path.basename(fnames[i]).rstrip(args.extension)
+                fout.write(f"{base_fname}|{pred_str}\n")
+    else:
+        schema = [
+            {'name': 'ID', 'type_v3': {'type_name': 'optional', 'item': 'utf8'}},
+            {'name': 'huberts_phonemes', 'type_v3': {'type_name': 'optional', 'item': 'yson'}}
+        ]
+        f = open('/home/viliana-dev/.yt/token')
+        yt_token = f.read().strip()
+        f.close()
+        yt_client = yt.wrapper.YtClient('hahn', token=yt_token)
+        yt_client.create_table(args.out_quantized_file_path, attributes={"schema": schema})
+
+        def row_processor(source_rows: yt.wrapper.format.RowsIterator, n_rows: int) -> Iterable:
+            i = 0
+            for row in tqdm(source_rows, total=n_rows):
+                feats = features_batch[i]
+                assert row['ID'] == ids_batch[i], "Rows must have equal ID"
+                i += 1
+                pred = kmeans_model.predict(feats)
+                phones = huberts_utterance(pred)
+                yield {
+                    "ID": row["ID"],
+                    "huberts_phonemes": phones
+                }
+
+        print(f"Writing quantized predictions to {args.out_quantized_file_path}")
+        source_rows = yt_client.read_table(table=yt.wrapper.TablePath(args.manifest_path))
+        rows_generator = row_processor(source_rows, n_rows=yt_client.row_count(args.manifest_path))
+        yt_client.write_table(args.out_quantized_file_path, rows_generator)
+        yt_client.run_sort(args.out_quantized_file_path, sort_by="ID")
+
 
 
 if __name__ == "__main__":
